7991aecc4e4504f3b73287b2113bd591c1987863
Darren Bishop <mail@darrenbishop.com>
2021-06-21 12:27:56 +0100

    Explored alternative offer representation; maybe more flexible going forward

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -3,31 +3,27 @@ import org.scalatest.matchers.should.Matchers
 
 class ShoppingCartSpec extends AnyFlatSpec with Matchers {
 
+  case class Offer(count: Int, reduction:Int)
+  case class ShoppingItem(price: Double, offer:Offer = Offer(1, 1))
+
   val priceList = Map(
-    "Apple" -> 0.60,
-    "Orange" -> 0.25
+    "Apple" -> ShoppingItem(0.60, Offer(2, 1)),
+    "Orange" -> ShoppingItem(0.25, Offer(3, 2))
   )
 
-  val discounts = Map(
-    "Apple" -> (2, 1),
-    "Orange" -> (3, 2)
-  )
-
-  def discount(n: Int, disc: (Int, Int)): Int = {
-    val (count, reduction) = disc
-    (n / count) * reduction + (n % count)
-  }
+  def discount(n: Int, offer:Offer): Int =
+    (n / offer.count) * offer.reduction + (n % offer.count)
 
   def calculate(goods: List[String]): String = {
     "£%.02f".format(
       goods
-        .groupMapReduce(identity)(_ => 1)(_ + _)
-        .map { case (product, count) => priceList(product) * discount(count, discounts(product)) }
+        .groupMapReduce(priceList)(_ => 1)(_ + _)
+        .map { case (ShoppingItem(price, offer), count) => price * discount(count, offer) }
         .sum)
   }
 
   behavior of "Shopping Cart"
-  note(f"Applying discounts: buy one, get one free on Apples (£${priceList("Apple")}%.02f); 3 for the price of 2 on Oranges (£${priceList("Orange")}%.02f)")
+  note(f"Applying discounts: buy one, get one free on Apples (£${priceList("Apple").price}%.02f); 3 for the price of 2 on Oranges (£${priceList("Orange").price}%.02f)")
 
   {
     val inputs = List("Apple", "Apple", "Orange", "Apple")
