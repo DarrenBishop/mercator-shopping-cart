4897ea41e82752a92550cb5b42d78d90f1b9a3a2
Darren Bishop <mail@darrenbishop.com>
2021-06-21 11:39:49 +0100

    Completed step 1 with passing test

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -3,8 +3,13 @@ import org.scalatest.matchers.should.Matchers
 
 class ShoppingCartSpec extends AnyFlatSpec with Matchers {
 
+  val priceList = Map(
+    "Apple" -> 0.60,
+    "Orange" -> 0.25
+  )
+
   def calculate(goods: List[String]): String = {
-    "£0.00"
+    "£%.02f".format(goods.map(priceList).fold(0d)(_ + _))
   }
 
   behavior of "Shopping Cary"
@@ -13,6 +18,39 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
     val inputs = List("Apple", "Apple", "Orange", "Apple")
 
     val expectation = "£2.05"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange", "Orange", "Orange")
+
+    val expectation = "£1.00"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Apple", "Apple", "Apple", "Apple")
+
+    val expectation = "£2.40"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = Nil
+
+    val expectation = "£0.00"
 
     it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
 
