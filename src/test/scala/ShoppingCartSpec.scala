89d60f327258b234339984f9b228a0878a6c37e1
Darren Bishop <mail@darrenbishop.com>
2021-06-21 11:51:16 +0100

    Implemented many failing tests

--- src/test/scala/ShoppingCartSpec.scala
+++ src/test/scala/ShoppingCartSpec.scala
@@ -12,12 +12,46 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
     "£%.02f".format(goods.map(priceList).fold(0d)(_ + _))
   }
 
-  behavior of "Shopping Cary"
+  behavior of "Shopping Cart"
+  note("Applying discounts: buy one, get one free on Apples; 3 for the price of 2 on Oranges")
 
   {
     val inputs = List("Apple", "Apple", "Orange", "Apple")
 
-    val expectation = "£2.05"
+    val expectation = "£1.85"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange")
+
+    val expectation = "£0.25"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange")
+
+    val expectation = "£0.50"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange", "Orange")
+
+    val expectation = "£0.50"
 
     it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
 
@@ -28,7 +62,73 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
   {
     val inputs = List("Orange", "Orange", "Orange", "Orange")
 
+    val expectation = "£0.75"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange", "Orange", "Orange", "Orange")
+
     val expectation = "£1.00"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+
+    val expectation = "£1.00"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Orange", "Orange", "Orange", "Orange", "Orange", "Orange", "Orange")
+
+    val expectation = "£1.25"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Apple")
+
+    val expectation = "£0.60"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Apple", "Apple")
+
+    val expectation = "£0.60"
+
+    it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
+
+      calculate(inputs) should equal(expectation)
+    }
+  }
+
+  {
+    val inputs = List("Apple", "Apple", "Apple")
+
+    val expectation = "£1.20"
 
     it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
 
@@ -39,7 +139,7 @@ class ShoppingCartSpec extends AnyFlatSpec with Matchers {
   {
     val inputs = List("Apple", "Apple", "Apple", "Apple")
 
-    val expectation = "£2.40"
+    val expectation = "£1.20"
 
     it should s"output '$expectation' for inputs `${inputs.mkString(", ")}`" in {
 
